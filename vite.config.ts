619293a8839558ca2863d7136f161c14dc3f4dce
cla6shade <cla6shade@gmail.com>
2023-09-23 22:43:49 +0900

    Chore: set modify aliases for resources
    컴포넌트와 asset, css 등을 모두 상대경로로 불러와 import 하는 경우 리소스의 정확한 경로를 알 수 없을 뿐더러 코드가 상당히 난잡해집니다.
    
    따라서 몇 가지 alias 등록을 통해 위 문제점을 해결하고자 하였습니다.
    
    @components: src/components
    @css: src/css
    @assets: src/assets (이미지, 아이콘 등의 파일 경로)
    
    사용 예시는 다음과 같습니다
    ```javascript
    import SomeComponent from '@components/SomeComponent.tsx'
    ```

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@components", replacement: "/src/components" },
+      { find: "@assets", replacement: "/src/assets" },
+      { find: "@css", replacement: "/src/css" },
+      { find: "@", replacement: "/src" },
+    ],
+  },
 })
