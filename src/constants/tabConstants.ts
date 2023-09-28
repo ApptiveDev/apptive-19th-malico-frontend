32cd8812cdc36fd66d81b1195ad6885d9587cd32
cla6shade <cla6shade@github.com>
2023-09-28 21:48:32 +0900

    Feat: Edit tab name, ids
    로그인, 회원가입, 마이페이지 등 단일 페이지는 react-router를 사용하여 핸들링하면 효율적일 것 같아 tabConstant의 항목들을 수정했습니다. tabConstant에는 접속 직후 홈 화면에서 네비게이션 바에 포함된 메뉴들을 선택하면 이동 가능한 탭들을 모아뒀습니다.

--- src/constants/tabConstants.ts
+++ src/constants/tabConstants.ts
@@ -1,7 +1,6 @@
 export default {
   TAB_ID_HOME: 0,
-  TAB_ID_LOGIN: 1,
-  TAB_ID_MYPAGE: 2,
-  TAB_ID_SETTINGS: 3,
-  names: ['홈', '로그인', '마이페이지', '설정'],
+  TAB_ID_CHATTING: 1,
+  TAB_ID_STYLING: 2,
+  names: ['홈', '채팅', '스타일링'],
 };
