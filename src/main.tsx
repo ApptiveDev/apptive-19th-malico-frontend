53c5db32e3b4d4af60f948959d4cf9ae5d20bfb1
cla6shade <cla6shade@github.com>
2023-09-28 18:36:12 +0900

    Feat: Add tabReducer
    현재 탭의 상태 관리를 위한 tab Reducer 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@components/App.tsx';
+
 import '@css/index.css';
-import {configureStore} from '@reduxjs/toolkit';
+// Reducers
 import authReducer from '@modules/authReducer.ts';
+import tabReducer from '@modules/tabReducer.ts';
+
+import {configureStore} from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    tab: tabReducer
   },
 });
 
