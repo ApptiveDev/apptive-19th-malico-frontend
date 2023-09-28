0c477b802d0d4a271ea12a7dcb3d26ba0c73e55c
cla6shade <cla6shade@github.com>
2023-09-28 15:13:13 +0900

    Feat: Add Redux store Provider for components
    redux에서 지원하던 createStore 대신 @reduxjs/toolkit에 내장된 configureStore 메소드 사용

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@components/App.tsx';
 import '@css/index.css';
+import {configureStore} from '@reduxjs/toolkit';
+import authReducer from '@modules/auth.ts';
+import {Provider} from 'react-redux';
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
