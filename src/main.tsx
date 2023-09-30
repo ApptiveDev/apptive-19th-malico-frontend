09546605d6ee979f643e7ceb6b2ea2a45012b5d5
cla6shade <cla6shade@github.com>
2023-09-30 20:03:57 +0900

    Rename: App to Home
    App 컴포넌트의 이름을 Home으로 변경. 이전 커밋에서의 변경 사항 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@components/App.tsx';
+import Home from '@components/Home.tsx';
 
 import '@css/index.css';
 
 import {rootStore} from '@/modules';
 import {Provider} from 'react-redux';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={rootStore}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
