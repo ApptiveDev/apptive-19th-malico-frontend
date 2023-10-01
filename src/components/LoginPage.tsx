050ca9fc0be7d187b16c2d4657a913ff164730d5
cla6shade <cla6shade@gmail.com>
2023-10-02 02:40:04 +0900

    Feat: Add LoginPage.tsx

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,8 +1,28 @@
-const LoginPage = () => {
+import PageContainer from '@components/container/PageContainer.tsx';
+import {useSelector} from 'react-redux';
+
+import {RootState} from '@/modules';
+
+import loginLogo from '@assets/images/login_logo.png';
+import {ReactNode} from 'react';
+import {Navigate} from 'react-router-dom';
+
+const LoginPage = (): ReactNode => {
+  const authenticated = useSelector((state: RootState) =>
+    state.auth.authenticated,
+  );
+
+  if (authenticated) {
+    return <Navigate to={'/'} />;
+  }
+
   return (
-    <>
-      <p>Login Page</p>
-    </>
+    <PageContainer>
+      <div className="flex justify-center mt-[100px]">
+        <img className="w-[183px] h-183px"
+          src={loginLogo}></img>
+      </div>
+    </PageContainer>
   );
 };
 
