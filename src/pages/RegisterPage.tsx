491af6937bdae464f63cea1224056c95b3928644
cla6shade <cla6shade@gmail.com>
2023-10-03 19:39:52 +0900

    Feat: Add Navbar on register page

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,8 +1,13 @@
+import PageContainer from '@components/container/PageContainer.tsx';
+import Navbar from '@components/navbar/Navbar.tsx';
+
 const RegisterPage = () => {
   return (
-    <>
-      <p>Register Page</p>
-    </>
+    <PageContainer>
+      <Navbar title='가입 유형 선택' hasBackwardButton={true} hasProgressBar={true}
+        progressNumerator={1} progressDominator={4} >
+      </Navbar>
+    </PageContainer>
   );
 };
 
