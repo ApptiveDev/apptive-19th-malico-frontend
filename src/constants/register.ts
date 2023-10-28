c6a6ee9d0f6eba9aa48e8cb870a4a101772376e6
cla6shade <cla6shade@github.com>
2023-10-28 19:58:50 +0900

    Feat: Add some constants for Register page

--- src/constants/register.ts
+++ src/constants/register.ts
@@ -1,4 +1,25 @@
 const registerConstants = {
+  page_nums: {
+    PAGE_REGISTER_SELECTION: 1,
+    PAGE_USER_AGREEMENT: 2,
+    PAGE_AUTHORIZATION: 3,
+    PAGE_INPUT_INFORMATION: 4,
 
+    PAGE_EULA: 5,
+    PAGE_PRIVACY_POLICY: 6,
+
+    PAGE_REGISTRATION_COMPLETE: 7,
+  },
+  page_titles: [
+    '',
+    '가입유형 선택',
+    '이용약관',
+    '본인 인증',
+    '계정 정보 입력',
+    '이용약관 상세',
+    '개인정보취급방침',
+    '회원가입 완료'],
+  max_pages: 4,
+  page_start: 1,
 };
 export default registerConstants;
