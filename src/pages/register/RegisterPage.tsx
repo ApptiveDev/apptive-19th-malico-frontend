80f8350a8fe1e82ad3217fa8e3272af8e19f6f24
cla6shade <cla6shade@github.com>
2023-11-02 00:41:38 +0900

    Feat: Modify Layout of RegisterPage
    네비게이션 바와 스크롤러블 컨테이너, 푸터로 구성 완료. 이제 라우팅 처리랑 안의 내용만 수정하면 끝

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -4,11 +4,13 @@ import {useState} from 'react';
 import Constants from '@/constants';
 import StickyFooter from '@components/footer/StickyFooter.tsx';
 import ResponsiveContainer from '@components/container/ResponsiveContainer.tsx';
+import ScrollableContainer from '@components/container/ScrollableContainer.tsx';
 
 const RegisterPage = () => {
   const [displayProgressbar/* , setDisplayProgressbar*/] = useState(true);
   const [currentProgress/* , setCurrentProgress*/] =
     useState(Constants.register.page_start);
+
   return (
     <PageContainer>
 
@@ -17,10 +19,12 @@ const RegisterPage = () => {
         hasProgressBar={displayProgressbar} progressNumerator={currentProgress}
         progressDominator={Constants.register.max_pages}>
       </Navbar>
-      <ResponsiveContainer>
-        <StickyFooter>
-        </StickyFooter>
-      </ResponsiveContainer>
+      <ScrollableContainer>
+        <ResponsiveContainer>
+        </ResponsiveContainer>
+      </ScrollableContainer>
+      <StickyFooter>
+      </StickyFooter>
     </PageContainer>
   );
 };
