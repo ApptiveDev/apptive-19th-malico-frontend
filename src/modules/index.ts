30d493983f5b3e1d7073deb03e987fa851ab241f
cla6shade <cla6shade@gmail.com>
2023-11-09 01:22:27 +0900

    Feat: Add reducer for register page

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '@modules/authReducer.ts';
+import registerReducer from '@modules/registerReducer.ts';
 
 export const rootStore = configureStore({
   reducer: {
     auth: authReducer,
+    register: registerReducer,
   },
 });
 export type RootState = ReturnType<typeof rootStore.getState>;
