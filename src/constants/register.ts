0b6996b6c19531be9048e8d34051f38f3aafe478
cla6shade <cla6shade@gmail.com>
2023-11-17 02:12:22 +0900

    Refactor: Modify getters for various constants

--- src/constants/register.ts
+++ src/constants/register.ts
@@ -21,19 +21,27 @@ const registerConstants = {
     '회원가입 완료'],
   max_pages: 4,
   page_start: 1,
+  progresses: [
+    null, '#register-selection',
+    '#user-agreement',
+    '#authorization',
+    '#input-information',
+    '#tos', '#privacy-policy',
+    '#complete'],
   getProgressFromHash: (hash: string) => {
-    const progress: {[key: string]: number} = {
-      '#register-selection': 1,
-      '#user-agreement': 2,
-      '#authorization': 3,
-      '#input-information': 4,
-
-      '#tos': 5,
-      '#privacy-policy': 6,
-
-      '#complete': 7,
-    };
-    return progress[hash] ?? 1;
+    const progresses = registerConstants.progresses;
+    return progresses.indexOf(hash) === -1 ? 1 : progresses.indexOf(hash);
+  },
+  getNextProgress: (currentProgress: number) => {
+    const pageNums = registerConstants.page_nums;
+    if (currentProgress < pageNums.PAGE_INPUT_INFORMATION) {
+      return currentProgress + 1;
+    }
+    return 0;
+  },
+  getNextUrlHash: (currentProgress: number): string => {
+    return <string>registerConstants.progresses[registerConstants.getNextProgress(
+      currentProgress)];
   },
   infos: {
     GENDER_MALE: '남',
