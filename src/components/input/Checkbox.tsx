50c12475c7486ec5a2cc3dd8e5806cf5db1046b7
cla6shade <cla6shade@gmail.com>
2023-11-21 00:02:24 +0900

    Feat: Set checked value controlled by parent component

--- src/components/input/Checkbox.tsx
+++ src/components/input/Checkbox.tsx
@@ -1,15 +1,9 @@
-import {useEffect, useState} from 'react';
-
 interface CheckboxProps {
-  initialState: boolean;
-  onChange?: (checked: boolean) => void;
+  checked: boolean;
+  setChecked: (checked: boolean) => void;
 }
 
-const Checkbox = ({initialState, onChange}: CheckboxProps) => {
-  const [checked, setChecked] = useState(initialState);
-  useEffect(() => {
-    setChecked(initialState);
-  }, [initialState]);
+const Checkbox = ({checked, setChecked}: CheckboxProps) => {
   return (
     <>
       <div
@@ -17,16 +11,13 @@ const Checkbox = ({initialState, onChange}: CheckboxProps) => {
           'flex justify-center items-center rounded-full w-[24px] h-[24px] bg-primary' :
           'flex justify-center items-center rounded-full w-[24px] h-[24px] bg-middle_grey'}
         onClick={() => {
-          if (onChange) {
-            onChange(checked);
-          }
-          setChecked(!checked);
+          setChecked(! checked);
         }}>
         <svg width='13' height='10' viewBox='0 0 14 10' fill='none'
           xmlns='http://www.w3.org/2000/svg'>
           <path d='M12.3327 1L4.99935 8.33333L1.66602 5'
             stroke={checked ? 'white' : 'black'}
-            stroke-width='2' stroke-linecap='round' stroke-linejoin='round' />
+            strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' />
         </svg>
       </div>
     </>
