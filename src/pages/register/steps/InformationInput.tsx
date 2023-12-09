b379ba724895c3faff76e68761a1667f0a4fc2b2
cla6shade <cla6shade@gmail.com>
2023-12-10 01:00:07 +0900

    Refactor: make validity check method clear

--- src/pages/register/steps/InformationInput.tsx
+++ src/pages/register/steps/InformationInput.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {ChangeEvent, useState} from 'react';
 import Input from '@components/input/Input.tsx';
 import {limitInputNumber} from '@/utils';
 import {useSelector} from 'react-redux';
@@ -6,74 +6,102 @@ import {RootState} from '@/modules';
 
 const InformationInput = () => {
   const registerState = useSelector((state: RootState) => state.register);
-  const [nicknameErrorMsg, setNicknameErrorMsg] =
-    useState<string | undefined>();
-  const [loginidErrorMsg, setLoginidErrorMsg] =
-    useState<string | undefined>();
-  const [passwordErrorMsg, setPasswordErrorMsg] =
-    useState<string | undefined>();
-  const [passConfirmErrorMsg, setPassConfirmErrorMsg] =
-    useState<string | undefined>();
-  return <>
-    <div className='py-6'>
-      <p className='text-[24px] font-semibold'>마리코에서 사용할</p>
-      <p className='text-[24px] font-semibold'>계정 정보를 입력해주세요.</p>
-    </div>
-    <p className='text-[18px] font-semibold mb-2'>닉네임</p>
-    <Input placeholder='한글, 영문, 숫자 중 자유롭게 2~10자 이내' id='register-auth-name'
-      onChange={(e) => {
-        limitInputNumber(e, 10);
-        if (!e.currentTarget.validity.valid) {
-          setNicknameErrorMsg('한글, 영문, 숫자 포함 2~10자 이내로 입력해주세요.');
-        } else {
-          setNicknameErrorMsg(undefined);
-        }
-      }} defaultValue={registerState.nickname ?? registerState.nickname}
-      errorMessage={nicknameErrorMsg}
-      pattern={'[가-힣a-zA-Z0-9]{2,10}'} />
-    <p className='text-[18px] font-semibold mb-2 mt-[16px]'>아이디</p>
-    <Input placeholder='영문 소문자, 숫자 포함 6~20자 이내' id='register-auth-name'
-      onChange={(e) => {
-        limitInputNumber(e, 20);
-        if (!e.currentTarget.validity.valid) {
-          setLoginidErrorMsg('영문 소문자, 숫자 포함 6~20자 이내로 입력해주세요.');
-        } else {
-          setLoginidErrorMsg(undefined);
-        }
-      }} defaultValue={registerState.loginid ?? registerState.loginid}
-      errorMessage={loginidErrorMsg}
-      pattern={'[a-z0-9]{6,20}'} />
 
-    <p className='text-[18px] font-semibold mb-2 mt-[16px]'>비밀번호</p>
-    <Input placeholder='영문, 숫자, 특수문자 포함 8~20자 이내' id='register-auth-name'
-      onChange={(e) => {
-        limitInputNumber(e, 20);
-        if (!e.currentTarget.validity.valid) {
-          setPasswordErrorMsg('영문 소문자, 숫자 포함 6~20자 이내로 입력해주세요.');
-        } else {
-          setPasswordErrorMsg(undefined);
-        }
-      }} defaultValue={registerState.loginid ?? registerState.loginid}
-      errorMessage={passwordErrorMsg}
-      pattern={'[a-zA-Z0-9!@#$%^&*()_+]{8,20}'}
-      type={'password'} />
+  const [errorMessages, setErrorMessages] = useState({
+    nickname: undefined,
+    loginid: undefined,
+    password: undefined,
+    passConfirm: undefined,
+  });
+  const [gender, setGender] = useState<string>('');
+  const [inputInfos, setInputInfos] = useState({
+    nickname: undefined,
+    loginid: undefined,
+    password: undefined,
+    passConfirm: undefined,
+    gender: undefined,
+  });
 
-    <p className='text-[18px] font-semibold mb-2 mt-[16px]'>비밀번호확인</p>
-    <Input placeholder='비밀번호를 한 번 더 입력해주세요.' id='register-auth-name'
-      onChange={(e) => {
-        limitInputNumber(e, 20);
-        if (!e.currentTarget.validity.valid) {
-          setPassConfirmErrorMsg('영문 소문자, 숫자 포함 6~20자 이내로 입력해주세요.');
-        } else {
-          setPassConfirmErrorMsg(undefined);
-        }
-      }} defaultValue={registerState.loginid ?? registerState.loginid}
-      errorMessage={passConfirmErrorMsg}
-      pattern={'[a-zA-Z0-9!@#$%^&*()_+]{8,20}'}
-      type={'password'}
-    />
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>, type: string,
+    maxLength: number, errorMsg: string) => {
+    const target = e.target;
+    limitInputNumber(e, maxLength);
+    if (! target.validity.valid) {
+      setErrorMessages((prev) => ({
+        ...prev,
+        [type]: errorMsg,
+      }));
+    }
+  };
 
-    <p className='text-[18px] font-semibold mb-2 mt-[16px]'>성별</p>
-  </>;
+  return (
+    <>
+      <div className='p-6'>
+        <p className='text-[24px] font-semibold'>마리코에서 사용할</p>
+        <p className='text-[24px] font-semibold'>계정 정보를 입력해주세요.</p>
+      </div>
+      <div className='px-6'>
+        <p className='text-[18px] font-semibold mb-2'>닉네임</p>
+        <Input
+          errorMessage={errorMessages.nickname}
+          placeholder='한글, 영문, 숫자 중 자유롭게 2~10자 이내'
+          id='register-auth-nickname'
+          onChange={(e) => handleInputChange(e, 'nickname',
+            10, '한글, 영문, 숫자 포함 2~10자 이내로 입력해주세요.')}
+          pattern={'[가-힣a-zA-Z0-9]{2,10}'}
+        />
+        <p className='text-[18px] font-semibold mb-2 mt-[16px]'>아이디</p>
+        <Input
+          errorMessage={errorMessages.loginid}
+          placeholder='영문 소문자, 숫자 포함 6~20자 이내'
+          id='register-auth-userid'
+          onChange={(e) => handleInputChange(e, 'loginid',
+            20, '영문 소문자, 숫자 포함 6~20자 이내로 입력해주세요.')}
+          pattern={'[a-z0-9]{6,20}'}
+        />
+        <p className='text-[18px] font-semibold mb-2 mt-[16px]'>비밀번호</p>
+        <Input
+          errorMessage={errorMessages.password}
+          placeholder='영문, 숫자, 특수문자 포함 8~20자 이내'
+          id='register-auth-password'
+          type='password'
+          onChange={(e) => handleInputChange(e, 'password',
+            20, '영문, 숫자, 특수문자 포함 8~20자 이내로 입력해주세요.')}
+          pattern={'^[A-Za-z\\d@$!%*?&]{8,20}$'}
+        />
+        <p className='text-[18px] font-semibold mb-2 mt-[16px]'>비밀번호확인</p>
+        <Input
+          errorMessage={errorMessages.passConfirm}
+          placeholder='비밀번호를 한 번 더 입력해주세요.'
+          id='register-auth-password_confirm'
+          type='password'
+          onChange={(e) => handleInputChange(e, 'passConfirm',
+            20, '비밀번호가 동일하지 않습니다.')}
+        />
+        <p className='text-[18px] font-semibold mb-2 mt-[16px]'>성별</p>
+        <div className='flex gap-2 w-full h-[40px]'>
+          <div className={gender == '남' ?
+            'grow flex items-center justify-center cursor-pointer text-[16px] font-bold ' +
+            'text-primary border-[2px] border-primary rounded-md h-full' :
+            'grow flex items-center justify-center cursor-pointer text-[16px] ' +
+            'text-dark_grey border-[2px] border-middle_grey rounded-lg h-full'}
+          onClick={() => setGender('남')}
+          >
+            <p>남성</p>
+          </div>
+
+          <div className={gender == '여' ?
+            'grow flex items-center justify-center cursor-pointer text-[16px] font-bold ' +
+            'text-primary border-[2px] border-primary rounded-md h-full' :
+            'grow flex items-center justify-center cursor-pointer text-[16px] ' +
+            'text-dark_grey border-[2px] border-middle_grey rounded-lg h-full'}
+          onClick={() => setGender('여')}>
+            <p>여성</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
+
 export default InformationInput;
