c9d71ed7d1707f3232e6238f0dfb3295eaaa86e2
cla6shade <cla6shade@gmail.com>
2023-12-10 00:59:16 +0900

    Design: add x-axis padding for registration pages

--- src/pages/register/steps/Authorization.tsx
+++ src/pages/register/steps/Authorization.tsx
@@ -25,89 +25,93 @@ const Authorization = () => {
     useState(registerState.authorized);
 
   return (<>
-    <div className='py-6'>
+    <div className='p-6'>
       <p className='text-[24px] font-semibold'>서비스 이용을 위해</p>
       <p className='text-[24px] font-semibold'>본인 인증을 진행해주세요.</p>
     </div>
-    <p className='text-[18px] font-semibold mb-2'>이름</p>
-    <Input placeholder='이름 입력' id='register-auth-name' onChange={(e) => {
-      limitInputNumber(e, 8);
-    }} defaultValue={registerState.name ?? registerState.name}
-    errorMessage={nameErrorMessage}/>
+    <div className='px-6'>
+      <p className='text-[18px] font-semibold mb-2'>이름</p>
+      <Input placeholder='이름 입력' id='register-auth-name' onChange={(e) => {
+        limitInputNumber(e, 8);
+      }} defaultValue={registerState.name ?? registerState.name}
+      errorMessage={nameErrorMessage} />
 
-    <p className='text-[18px] font-semibold mt-[16px] mb-2'>이메일</p>
-    <div className='flex h-[40px] gap-2'>
-      <div className='flex grow h-[40px]'>
-        <Input placeholder='이메일 입력'
-          type={'email'}
-          pattern='[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]{2,4}'
-          onChange={(e) => {
-            limitInputNumber(e, 85);
-            if (!e.target.validity.valid) {
-              setEmailErrorMessage('이메일을 다시 확인해주세요.');
-              setSendDisabled(true);
-            } else {
-              setEmailErrorMessage(undefined);
-              setSendDisabled(false);
-            }
-          }}
-          errorMessage={emailErrorMessage}
-          id='register-auth-email'
-          disabled={emailSent}
-          defaultValue={registerState.email ?? registerState.email}
-        />
-      </div>
-      <Button label='인증번호 전송' style={{
-        height: '40px',
-        fontSize: '16px',
-        flexShrink: 0,
-      }} disabled={sendDisabled}
-      onClick={() => {
-        if (!sendDisabled) {
-          const target: HTMLInputElement =
-              (document.getElementById(
-                'register-auth-email') as HTMLInputElement);
-          const emailValue = target.value;
-          const nameInput =
-            (document.getElementById('register-auth-name') as HTMLInputElement);
-          const name = nameInput.value;
-          if (name === '') {
-            setNameErrorMessage('올바른 입력을 입력해주세요.');
-            return;
-          }
-
-          dispatch(setRegisterInfo({name, email: emailValue}));
-
-          setSendDisabled(true);
-          setEmailSent(true);
-        }
-      }} />
-    </div>
-    {emailSent ? <>
-      <p className='text-blue-500 text-[14px] mt-[8px] ml-[4px]'>
-        인증번호가 전송되었습니다.</p>
-      <div className='flex h-[40px] mt-[16px] gap-2'>
+      <p className='text-[18px] font-semibold mt-[16px] mb-2'>이메일</p>
+      <div className='flex h-[40px] gap-2'>
         <div className='flex grow h-[40px]'>
-          <Input placeholder={registerState.authorized ? '인증 완료': '인증번호를 입력해주세요.'}
+          <Input placeholder='이메일 입력'
+            type={'email'}
+            pattern='[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]{2,4}'
             onChange={(e) => {
-              limitInputNumber(e, 6);
+              limitInputNumber(e, 85);
+              if (!e.target.validity.valid) {
+                setEmailErrorMessage('이메일을 다시 확인해주세요.');
+                setSendDisabled(true);
+              } else {
+                setEmailErrorMessage(undefined);
+                setSendDisabled(false);
+              }
             }}
-            errorMessage={authnumErrorMessage}
-            id='register-auth-number'
-            disabled={registerState.authorized}
+            errorMessage={emailErrorMessage}
+            id='register-auth-email'
+            disabled={emailSent}
+            defaultValue={registerState.email ?? registerState.email}
           />
         </div>
-        <Button label='인증번호 확인' style={{
+        <Button label='인증번호 전송' style={{
           height: '40px',
           fontSize: '16px',
           flexShrink: 0,
-        }} disabled={registerState.authorized}
+        }} disabled={sendDisabled}
         onClick={() => {
-          alert('인증이 완료되었습니다.');
-          dispatch(setRegisterInfo({authorized: true}));
+          if (!sendDisabled) {
+            const target: HTMLInputElement =
+                (document.getElementById(
+                  'register-auth-email') as HTMLInputElement);
+            const emailValue = target.value;
+            const nameInput =
+                (document.getElementById(
+                  'register-auth-name') as HTMLInputElement);
+            const name = nameInput.value;
+            if (name === '') {
+              setNameErrorMessage('올바른 입력을 입력해주세요.');
+              return;
+            }
+
+            dispatch(setRegisterInfo({name, email: emailValue}));
+
+            setSendDisabled(true);
+            setEmailSent(true);
+          }
         }} />
       </div>
-    </> : null}
+      {emailSent ? <>
+        <p className='text-blue-500 text-[14px] mt-[8px] ml-[4px]'>
+          인증번호가 전송되었습니다.</p>
+        <div className='flex h-[40px] mt-[16px] gap-2'>
+          <div className='flex grow h-[40px]'>
+            <Input
+              placeholder={registerState.authorized ? '인증 완료' : '인증번호를 입력해주세요.'}
+              onChange={(e) => {
+                limitInputNumber(e, 6);
+              }}
+              errorMessage={authnumErrorMessage}
+              id='register-auth-number'
+              disabled={registerState.authorized}
+            />
+          </div>
+          <Button label='인증번호 확인' style={{
+            height: '40px',
+            fontSize: '16px',
+            flexShrink: 0,
+          }} disabled={registerState.authorized}
+          onClick={() => {
+            alert('인증이 완료되었습니다.');
+            dispatch(setRegisterInfo({authorized: true}));
+          }} />
+        </div>
+      </> : null}
+    </div>
   </>);
 };
 
--- src/pages/register/steps/UserAgreement.tsx
+++ src/pages/register/steps/UserAgreement.tsx
@@ -38,41 +38,44 @@ const UserAgreement = () => {
       <p className='text-[24px] font-semibold'>마리코 서비스 이용약관에</p>
       <p className='text-[24px] font-semibold'>동의해주세요.</p>
     </div>
-    <div className='mt-[40px] h-[256px] w-full flex flex-col items-center'>
-      <div className={agreeAllClass}
-        onClick={() => {
-          setPrvPolicyAgreed(!checkedAll);
-          setTosAgreed(!checkedAll);
-        }}
-      >
-        <div className='flex ml-[16px] mr-[12px] items-center
+    <div className='px-6'>
+      <div className='mt-[40px] h-[256px] w-full flex flex-col items-center'>
+        <div className={agreeAllClass}
+          onClick={() => {
+            setPrvPolicyAgreed(!checkedAll);
+            setTosAgreed(!checkedAll);
+          }}
+        >
+          <div className='flex ml-[16px] mr-[12px] items-center
         justify-center h-[24px] w-[24px]'>
-          <svg width='14' height='10' viewBox='0 0 14 10' fill='none'
-            xmlns='http://www.w3.org/2000/svg'>
-            <path d='M12.3327 1L4.99935 8.33333L1.66602 5'
-              stroke={checkedAll ? 'white' : 'black'}
-              strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'
-              ref={strokeRef} />
-          </svg>
-        </div>
-        <p className='text-[16px]'>전체 약관 동의합니다</p>
-      </div>
-      <div className='flex w-full items-center flex-col mt-[8px]'>
-        <div className='w-full h-[40px] pl-[16px] flex items-center'>
-          <Checkbox checked={tosAgreed} setChecked={setTosAgreed} />
-          <div className='border border-primary text-primary
-          text-[12px] w-[33px] rounded-full flex justify-center items-center mx-[8px]'>
-            필수
+            <svg width='14' height='10' viewBox='0 0 14 10' fill='none'
+              xmlns='http://www.w3.org/2000/svg'>
+              <path d='M12.3327 1L4.99935 8.33333L1.66602 5'
+                stroke={checkedAll ? 'white' : 'black'}
+                strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'
+                ref={strokeRef} />
+            </svg>
           </div>
-          <p className='text-[14px]'>이용약관 동의</p>
+          <p className='text-[16px]'>전체 약관 동의합니다</p>
         </div>
-        <div className='w-full h-[40px] pl-[16px] flex items-center'>
-          <Checkbox checked={prvPolicyAgreed} setChecked={setPrvPolicyAgreed} />
-          <div className='border border-primary text-primary
+        <div className='flex w-full items-center flex-col mt-[8px]'>
+          <div className='w-full h-[40px] pl-[16px] flex items-center'>
+            <Checkbox checked={tosAgreed} setChecked={setTosAgreed} />
+            <div className='border border-primary text-primary
           text-[12px] w-[33px] rounded-full flex justify-center items-center mx-[8px]'>
-            필수
+              필수
+            </div>
+            <p className='text-[14px]'>이용약관 동의</p>
           </div>
-          <p className='text-[14px]'>개인정보 수집 및 이용 동의</p>
+          <div className='w-full h-[40px] pl-[16px] flex items-center'>
+            <Checkbox checked={prvPolicyAgreed}
+              setChecked={setPrvPolicyAgreed} />
+            <div className='border border-primary text-primary
+          text-[12px] w-[33px] rounded-full flex justify-center items-center mx-[8px]'>
+              필수
+            </div>
+            <p className='text-[14px]'>개인정보 수집 및 이용 동의</p>
+          </div>
         </div>
       </div>
     </div>
