30aacb3c292b695dedf1ab683f188a91912c1cfe
cla6shade <cla6shade@gmail.com>
2023-12-16 00:49:02 +0900

    Feat: Implementation of Information input page

--- src/pages/register/steps/InformationInput.tsx
+++ src/pages/register/steps/InformationInput.tsx
@@ -1,38 +1,77 @@
-import {ChangeEvent, useState} from 'react';
+import {ChangeEvent, useEffect, useState} from 'react';
 import Input from '@components/input/Input.tsx';
 import {limitInputNumber} from '@/utils';
-// import {useSelector} from 'react-redux';
-// import {RootState} from '@/modules';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '@/modules';
+import {RegisterState, setRegisterInfo} from '@modules/registerReducer.ts';
+
+interface RegisterInputErrorMessages {
+  nickname?: string,
+  loginid?: string,
+  password?: string,
+  passwordConfirm?: string,
+}
 
 const InformationInput = () => {
-  // const registerState = useSelector((state: RootState) => state.register);
-
-  const [errorMessages, setErrorMessages] = useState({
-    nickname: undefined,
-    loginid: undefined,
-    password: undefined,
-    passConfirm: undefined,
-  });
+  const registerState = useSelector((state: RootState) => state.register);
+  const dispatch = useDispatch();
+  const [errorMessages, setErrorMessages] =
+    useState<RegisterInputErrorMessages>({
+      nickname: undefined,
+      loginid: undefined,
+      password: undefined,
+      passwordConfirm: undefined,
+    });
   const [gender, setGender] = useState<string>('');
-  // const [inputInfos, setInputInfos] = useState({
-  //   nickname: undefined,
-  //   loginid: undefined,
-  //   password: undefined,
-  //   passConfirm: undefined,
-  //   gender: undefined,
-  // });
+  const [password, setPassword] = useState<string>('');
+  const [passwordConfirm, setPasswordConfirm] = useState<string>('');
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>, type: string,
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>,
+    type: keyof RegisterState,
     maxLength: number, errorMsg: string) => {
     const target = e.target;
     limitInputNumber(e, maxLength);
-    if (! target.validity.valid) {
+    if (!target.validity.valid) {
       setErrorMessages((prev) => ({
         ...prev,
         [type]: errorMsg,
       }));
+      if (typeof registerState[type] !== 'undefined') {
+        dispatch(setRegisterInfo({[type]: undefined}));
+      }
+    } else {
+      setErrorMessages((prev) => ({
+        ...prev,
+        [type]: undefined,
+      }));
+      dispatch(setRegisterInfo({[type]: target.value}));
     }
   };
+
+  useEffect(() => {
+    if (password !== passwordConfirm) {
+      if (typeof errorMessages.passwordConfirm === 'undefined') {
+        setErrorMessages((prev) => ({
+          ...prev,
+          'passwordConfirm': '비밀번호가 동일하지 않습니다.',
+        }));
+      }
+      dispatch(setRegisterInfo({
+        password: undefined,
+        passwordConfirm: undefined,
+      }));
+    } else {
+      if (typeof errorMessages.passwordConfirm === 'undefined') {
+        setErrorMessages((prev) => ({
+          ...prev,
+          'passwordConfirm': undefined,
+        }));
+      }
+      dispatch(setRegisterInfo({
+        password, passwordConfirm,
+      }));
+    }
+  }, [password, passwordConfirm]);
 
   return (
     <>
@@ -65,37 +104,49 @@ const InformationInput = () => {
           placeholder='영문, 숫자, 특수문자 포함 8~20자 이내'
           id='register-auth-password'
           type='password'
-          onChange={(e) => handleInputChange(e, 'password',
-            20, '영문, 숫자, 특수문자 포함 8~20자 이내로 입력해주세요.')}
+          onChange={(e) => {
+            setPassword(e.target.value);
+            handleInputChange(e, 'password',
+              20, '영문, 숫자, 특수문자 포함 8~20자 이내로 입력해주세요.');
+          }}
           pattern={'^[A-Za-z\\d@$!%*?&]{8,20}$'}
         />
         <p className='text-[18px] font-semibold mb-2 mt-[16px]'>비밀번호확인</p>
         <Input
-          errorMessage={errorMessages.passConfirm}
+          errorMessage={errorMessages.passwordConfirm}
           placeholder='비밀번호를 한 번 더 입력해주세요.'
           id='register-auth-password_confirm'
           type='password'
-          onChange={(e) => handleInputChange(e, 'passConfirm',
-            20, '비밀번호가 동일하지 않습니다.')}
+          onChange={(e) => {
+            setPasswordConfirm(e.target.value);
+            handleInputChange(e, 'passwordConfirm',
+              20, '비밀번호가 동일하지 않습니다.');
+          }}
         />
         <p className='text-[18px] font-semibold mb-2 mt-[16px]'>성별</p>
         <div className='flex gap-2 w-full h-[40px]'>
           <div className={gender == '남' ?
             'grow flex items-center justify-center cursor-pointer text-[16px] font-bold ' +
-            'text-primary border-[2px] border-primary rounded-md h-full' :
+              'text-primary border-[2px] border-primary rounded-md h-full' :
             'grow flex items-center justify-center cursor-pointer text-[16px] ' +
-            'text-dark_grey border-[2px] border-middle_grey rounded-lg h-full'}
-          onClick={() => setGender('남')}
+              'text-dark_grey border-[2px] border-middle_grey rounded-lg h-full'}
+          onClick={() => {
+            setGender('남');
+            dispatch(setRegisterInfo({'gender': '남'}));
+          }}
           >
             <p>남성</p>
           </div>
 
           <div className={gender == '여' ?
             'grow flex items-center justify-center cursor-pointer text-[16px] font-bold ' +
-            'text-primary border-[2px] border-primary rounded-md h-full' :
+              'text-primary border-[2px] border-primary rounded-md h-full' :
             'grow flex items-center justify-center cursor-pointer text-[16px] ' +
-            'text-dark_grey border-[2px] border-middle_grey rounded-lg h-full'}
-          onClick={() => setGender('여')}>
+              'text-dark_grey border-[2px] border-middle_grey rounded-lg h-full'}
+          onClick={() => {
+            setGender('여');
+            dispatch(setRegisterInfo({'gender': '여'}));
+          }}>
             <p>여성</p>
           </div>
         </div>
