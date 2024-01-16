aae573993564545b4966bd3dbc8db159f752274f
cla6shade <cla6shade@gmail.com>
2024-01-16 23:28:03 +0900

    Fix: unused variable 주석 처리

--- src/components/footer/FooterBar.tsx
+++ src/components/footer/FooterBar.tsx
@@ -2,7 +2,7 @@ type FooterBarProps = {
   currentMenu: number
 }
 
-const FooterBar = ({currentMenu}: FooterBarProps) => {
+const FooterBar = ({/* currentMenu */}: FooterBarProps) => {
   return <div className='w-full h-[85px]'>
     <div className='mt-[31px] w-full h-[54px] flex justify-between items-center'>
 
