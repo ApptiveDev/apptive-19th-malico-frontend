4f8f903ad8b99e952f2b5f6d20ef31792dc3a90b
cla6shade <cla6shade@github.com>
2024-01-18 16:47:50 +0900

    Feat: find-id 구현

--- src/pages/findId/FindIdPage.tsx
+++ src/pages/findId/FindIdPage.tsx
@@ -1,104 +1,188 @@
-import ResponsiveContainer from '@components/container/ResponsiveContainer.tsx';
-import StickyFooter from '@components/footer/StickyFooter.tsx';
-import PageContainer from '@components/container/PageContainer.tsx';
-import Navbar from '@components/navbar/Navbar.tsx';
-import {ChangeEvent, useState} from 'react';
-import {limitInputNumber} from '@/utils';
+import {useSelector} from 'react-redux';
+import {useState} from 'react';
 import Input from '@components/input/Input.tsx';
 import Button from '@components/button/Button.tsx';
-import FindIdResult from '@pages/findId/FindIdResult.tsx';
-import SmallButton from '@components/button/SmallButton.tsx';
+import {limitInputNumber} from '@/utils';
+import {RootState} from '@/modules';
 import PageCaption from '@components/text/PageCaption.tsx';
+import PageContainer from '@components/container/PageContainer.tsx';
+import Navbar from '@components/navbar/Navbar.tsx';
+import ScrollableContainer from '@components/container/ScrollableContainer.tsx';
+import ResponsiveContainer from '@components/container/ResponsiveContainer.tsx';
+import StickyFooter from '@components/footer/StickyFooter.tsx';
+import FindIdResult from '@pages/findId/FindIdResult.tsx';
+import axiosInstance from '@utils/AxiosInstance.ts';
 
 const FindIdPage = () => {
-  const [/* name */, setName] = useState('');
-  const [/* email */, setEmail] = useState('');
-  const [/* authCode */, setAuthCode] = useState('');
-  const [isSended, setIsSended] = useState(false);
-  const [isConfirmed, setIsConfirmed] = useState(false);
-  const [isSuccesed, setIsSuccessed] = useState(false);
+  const registerState = useSelector((state: RootState) => state.register);
+  const [emailErrorMessage, setEmailErrorMessage] =
+    useState<string | undefined>(undefined);
+  const [firstSendComplete, setFirstSendComplete] =
+    useState(false);
+  const [sendDisabled, setSendDisabled] = useState(true);
+  const [authnumErrorMessage/* setAuthnumErrorMessage*/] =
+    useState<string | undefined>(undefined);
+  const [verificationCode, setVerificationCode] = useState<string>();
 
-  return (
-    <PageContainer>
-      <Navbar title='아이디 찾기'
-        hasBackwardButton={true}>
-      </Navbar>
-      <ResponsiveContainer>
-        <div>
-          {
-            isSuccesed ? (
-              <FindIdResult/>
-            ) : (
-              <div className='flex flex-col h-[calc(100vh-72px)]'>
-                <PageCaption lines={['아이디를 찾기 위해', '본인 인증을 진행해 주세요.']}/>
-                <div className='flex flex-col px-6'>
-                  <div className='mb-8 mt-[8px]'>
-                    <p className='text-[18px] mb-1 font-bold'>이름</p>
-                    <Input placeholder='이름 입력'
-                      onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                        const inputName = e.target.value;
-                        setName(inputName);
-                        limitInputNumber(e, 10); // 이름의 최대 글자수 제한: 4글자.
-                        // 사용자 정보 db 스키마가 정의되면 상수로서 정의하는 것이 좋을 것 같음
-                      }}></Input>
-                  </div>
-                  <div className='mb-8'>
-                    <p className='text-[18px] mb-1 font-bold'>이메일</p>
-                    <div className='flex items-center space-x-2'>
-                      <div className='flex grow items-center'>
-                        <Input
-                          placeholder='이메일 입력'
-                          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                            const inputEmail = e.target.value;
-                            setEmail(inputEmail);
-                            limitInputNumber(e, 10);
-                          }}></Input>
-                      </div>
-                      <div className='flex h-full items-center'>
-                        {isConfirmed ?
-                          (<SmallButton label={'인증 완료'}
-                            disabled={true}/>) :
-                          (<SmallButton label={isSended ? '재전송' : '인증번호 전송'}
-                            onClick={() => setIsSended(true)}/>)}
-                      </div>
-                    </div>
-                  </div>
-                  {(!isConfirmed && isSended) && (
-                    <div className='mb-8'>
-                      <p className='text-[18px] mb-1 font-bold'>인증번호</p>
-                      <div className='flex items-center space-x-2'>
-                        <div className='flex-grow'>
-                          <Input
-                            placeholder='인증번호 입력'
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                              const inputAuthCode = e.target.value;
-                              setAuthCode(inputAuthCode);
-                              limitInputNumber(e, 10);
-                            }}></Input>
-                        </div>
-                        <div className='flex'>
-                          <SmallButton label={'인증번호 확인'}
-                            onClick={() => setIsConfirmed(true)}
-                          />
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </div>
-                <div className='flex flex-col mt-auto mb-12 px-6 ' >
-                  <Button label="확인" onClick={() => setIsSuccessed(true)}
-                    disabled={! isConfirmed}
-                  />
-                </div>
-              </div>
-            )
-          }
+  const [emailSent, setEmailSent] =
+    useState(registerState.authorized);
+
+  const [email, setEmail] = useState<string>('');
+  const [authorized, setAuthorized] = useState<boolean>(false);
+  const [userId, setUserId] = useState<string>('');
+
+  const [idExists, setIdExists] = useState<boolean>(false);
+
+  const sendVerification = (email: string) => {
+    setFirstSendComplete(true);
+    setEmailSent(true);
+    setSendDisabled(false);
+
+    axiosInstance.post('/auth/user/search/verification-code', {email: email}).then(() => {
+      setFirstSendComplete(true);
+      setEmailSent(true);
+      setSendDisabled(false);
+    }).catch((err) => {
+      console.log(err);
+      if (err.response) {
+        setSendDisabled(false);
+        setFirstSendComplete(false);
+        setEmailSent(false);
+      }
+    });
+  };
+  const verifyCode = async () => {
+    try {
+      await axiosInstance.get('/auth/user/search/verification-code?code=' + verificationCode);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  };
+
+  const getResult = async () => {
+    try {
+      const res = await axiosInstance.get('/auth/user/search/id?code=' + verificationCode);
+      const userid = res.data.data;
+      setUserId(userid);
+      setAuthorized(true);
+      setIdExists(true);
+    } catch (e) {
+      console.log(e);
+      setAuthorized(true);
+      setUserId('');
+    }
+  };
+
+  const getFindIdForm = () => {
+    return (<><PageCaption lines={['아이디를 찾기 위해', '본인 인증을 진행해 주세요.']} />
+      <div className='px-6 mt-[8px]'>
+        <p className='text-[18px] font-semibold mt-[16px] mb-2'>이메일</p>
+        <div className='flex h-[40px] gap-2'>
+          <div className='flex grow h-[40px]'>
+            <Input placeholder='이메일 입력'
+              type={'email'}
+              pattern='[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-z]{2,4}'
+              onChange={(e) => {
+                limitInputNumber(e, 85);
+                if (!e.target.validity.valid) {
+                  setEmailErrorMessage('이메일을 다시 확인해주세요.');
+                  setSendDisabled(true);
+                } else {
+                  setEmailErrorMessage(undefined);
+                  setSendDisabled(false);
+                  setEmail(e.target.value);
+                }
+              }}
+              errorMessage={emailErrorMessage}
+              id='register-auth-email'
+              disabled={emailSent}
+              defaultValue={registerState.email ?? registerState.email} />
+          </div>
+          <Button label={firstSendComplete ? '재전송' : '인증번호 전송'} style={{
+            height: '40px',
+            fontSize: '16px',
+            flexShrink: 0,
+          }} disabled={sendDisabled}
+          onClick={() => {
+            sendVerification(email);
+          }} />
         </div>
+        {emailSent ? <>
+          <p className='text-blue-500 text-[14px] mt-[8px] ml-[4px]'>
+            인증번호가 전송되었습니다.</p>
+          <p className='text-[18px] font-semibold mt-[16px] mb-2'>인증번호</p>
+          <div className='flex h-[40px] gap-2'>
+            <div className='flex grow h-[40px]'>
+              <Input
+                placeholder={registerState.authorized ?
+                  '인증 완료' :
+                  '인증번호를 입력해주세요.'}
+                onChange={(e) => {
+                  limitInputNumber(e, 6);
+                  setVerificationCode(e.target.value);
+                }}
+                errorMessage={authnumErrorMessage}
+                id='register-auth-number'
+                disabled={registerState.authorized} />
+            </div>
+            <Button label='인증번호 확인' style={{
+              height: '40px',
+              fontSize: '16px',
+              flexShrink: 0,
+            }} disabled={registerState.authorized}
+            onClick={async () => {
+              const authResult = await verifyCode();
+              if (authResult) {
+                alert('인증이 완료되었습니다.');
+                await getResult();
+              } else {
+                alert('인증에 실패했습니다. 인증번호를 다시 확인해 주세요.');
+              }
+            }} />
+          </div>
+        </> : null}
+      </div>
+    </>);
+  };
+
+  return (<PageContainer>
+    <Navbar title={'아이디 찾기'} hasBackwardButton={true}/>
+    <ScrollableContainer>
+      <ResponsiveContainer>
+        {authorized ? <FindIdResult idExists={idExists} userId={userId} /> : getFindIdForm()}
       </ResponsiveContainer>
-      <StickyFooter>
-      </StickyFooter>
-    </PageContainer>
-  );
+    </ScrollableContainer>
+    <StickyFooter>
+      {authorized ?
+        <div className='w-full h-full max-w-[400px] flex flex-col px-6'>
+          {authorized ?
+            idExists ?
+              <div className='w-full h-full max-w-[400px] flex flex-col px-6 py-6 gap-2'>
+                <Button label={'로그인'} onClick={() => {}}>
+                </Button>
+                <Button label={'비밀번호 찾기'} style={{
+                  backgroundColor: 'white',
+                  borderColor: 'black',
+                  color: 'black',
+                  border: '1px black solid',
+                }}></Button>
+              </div> :
+              <div className='w-full h-full max-w-[400px] flex flex-col px-6 py-6 gap-2'>
+                <Button label={'회원가입'} onClick={() => {}}>
+                </Button>
+                <Button label={'처음으로 돌아가기'} style={{
+                  backgroundColor: 'white',
+                  borderColor: 'black',
+                  color: 'black',
+                  border: '1px black solid',
+                }}></Button>
+              </div> : null
+          }
+        </div> : null
+      }
+    </StickyFooter>
+  </PageContainer>);
 };
 
 export default FindIdPage;
